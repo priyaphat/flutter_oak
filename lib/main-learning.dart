e3e052c539367451bb8a66985c4df0263a0f5713
OTAMOS <priyaphat_sriphueng@hotmail.com>
2022-03-03 17:35:21 +0700

    commit

---
+++ lib/main-learning.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'question.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<Home> {
+  int _questionIndex = 0;
+  String sum = 'true';
+  var answer = false;
+
+  void onPressedTomHank() {
+    print("TomHank +1");
+    // _questionIndex = _questionIndex + 1;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print('qusetionIndex : $_questionIndex');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var qusetions = ['ใครเป็นพ่อเธอ ? ', '.ใครเป็นแม่เธอ ?!'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first App"),
+        ),
+        body: Column(children: [
+          Question(qusetions[_questionIndex]),
+          RaisedButton(
+            onPressed: () => onPressedTomHank(), // arrow funtion
+            child: Text('RaisedButton'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print("live in japan");
+            }, // anonymous function
+            child: Text('Let\'s go japan'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              onPressedTomHank();
+              setState(() {
+                answer = false;
+                sum = 'flase';
+              });
+            },
+            child: Text('Waht your Name'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                answer = true;
+                sum = 'true';
+              });
+            },
+            child: Text('Who ?'),
+          ),
+          answer ? Question(sum) : Text(sum)
+        ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,102 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'transaction.dart';
 
-void main() {
-  runApp(Home());
-}
+void main() => runApp(Home());
 
-class Home extends StatefulWidget {
-  State<StatefulWidget> createState() {
-    return _MyApp();
-  }
-}
-
-class _MyApp extends State<Home> {
-  int _questionIndex = 0;
-  String sum = 'true';
-  var answer = false;
-
-  void onPressedTomHank() {
-    print("TomHank +1");
-    // _questionIndex = _questionIndex + 1;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print('qusetionIndex : $_questionIndex');
-  }
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var qusetions = ['ใครเป็นพ่อเธอ ? ', '.ใครเป็นแม่เธอ ?!'];
-    return MaterialApp(
-      home: Scaffold(
+    return MaterialApp(title: 'Flutter App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: 't1', title: 'กระเพาไก่', amount: 200, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'กระเพาเป็ด', amount: 220, date: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: Text("My first App"),
+          title: Text('App Page'),
         ),
-        body: Column(children: [
-          Question(qusetions[_questionIndex]),
-          RaisedButton(
-            onPressed: () => onPressedTomHank(), // arrow funtion
-            child: Text('RaisedButton'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              print("live in japan");
-            }, // anonymous function
-            child: Text('Let\'s go japan'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              onPressedTomHank();
-              setState(() {
-                answer = false;
-                sum = 'flase';
-              });
-            },
-            child: Text('Waht your Name'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                answer = true;
-                sum = 'true';
-              });
-            },
-            child: Text('Who ?'),
-          ),
-          answer ? Question(sum) : Text(sum)
-        ]),
-      ),
-    );
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: transaction.map((tx) {
+              return Card(
+                child: Container(
+                  height: 100,
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Expanded(
+                            child: Image.asset("assets/images/t1.png"),
+                            flex: 2,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: ListTile(
+                                  title: Text(
+                                      '${tx.id} - ${tx.title} - ${tx.amount} บาท'),
+                                  subtitle: Text('${tx.date}'),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 5,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TextButton(
+                                      child: Text("PLAY"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(
+                                      width: 8,
+                                    ),
+                                    TextButton(
+                                      child: Text("ADD TO QUEUE"),
+                                      onPressed: () {},
+                                    ),
+                                    SizedBox(
+                                      width: 8,
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        flex: 8,
+                      ),
+                    ],
+                  ),
+                ),
+                elevation: 8,
+                margin: EdgeInsets.all(10),
+              );
+            }).toList()));
   }
 }
